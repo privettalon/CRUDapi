63439d80c1dff375643c885f1922b5e8bf7273fa
Dmitry Kovalchuk <55485703+privettalon@users.noreply.github.com>
2021-08-03 14:55:37 +0300

    Add WebAPI

---
+++ WebApi/WebApi/Migrations/20210803113926_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    OrganizationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrganizationName = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar (50)", nullable: true),
+                    NumberOfStuff = table.Column<int>(type: "int", nullable: false),
+                    ContactNumber = table.Column<string>(type: "nvarchar (12)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.OrganizationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Organizations");
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/Organization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Organization
+    {
+        [Key]
+        public int OrganizationId { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string OrganizationName { get; set; }
+        [Column(TypeName = "nvarchar (50)")]
+        public string Location { get; set; }
+
+        public int NumberOfStuff { get; set; }
+        
+        [Column(TypeName = ("nvarchar (12)"))]
+        public string ContactNumber { get; set; }
+    }
+}
